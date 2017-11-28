07e7c1936782f99756e6f766eeb8d25ca4168f39
Arniss1 <34061874+Arniss1@users.noreply.github.com>
2017-11-28 13:35:25 +0000

    Add files via upload

---
+++ scatter.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# draw Scatter with 12 points
+X = [48,25,36,46,97,15,40,78,86,24,62,56]
+Y = [32,15,39,52,61,72,22,75,68,57,88,48]
+
+plt.scatter(X,Y)
+plt.title('Scatter of 12 points')
+plt.xlabel('X - axis')
+plt.ylabel('Y - axis')
+
+plt.show()
+
